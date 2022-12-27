21da15cb0581e0bd7b1379fda7869d0ffa9e95a3
SanithaLakshmi <121496351+SanithaLakshmi@users.noreply.github.com>
2022-12-27 18:25:06 +0530

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -6,20 +6,20 @@ using namespace std;
 vector<vector<int>> Subscriptions(int budget){
    vector<vector<int>> Combination;
    int TOI, Hindu, ET, BM, HT;
-   for (TOI = 0; TOI <= budget; TOI++) {
-      for (Hindu = 0; Hindu <= budget; Hindu++) {
-         for (ET = 0; ET <= budget; ET++) {
-            for (BM = 0; BM <= budget; BM++) {
-               for (HT = 0; HT <= budget; HT++) {
-                  if (TOI + Hindu + ET + BM + HT == budget) {
-                     vector<int> combination = {TOI, Hindu, ET, BM, HT};
-                     Combination.push_back(combination);
+            for (TOI = 0; TOI <= budget; TOI++) {
+              for (Hindu = 0; Hindu <= budget; Hindu++) {
+                  for (ET = 0; ET <= budget; ET++) {
+                      for (BM = 0; BM <= budget; BM++) {
+                        for (HT = 0; HT <= budget; HT++) {
+                            if (TOI + Hindu + ET + BM + HT == budget) {
+                              vector<int> combination = {TOI, Hindu, ET, BM, HT};
+                                Combination.push_back(combination);
+                         }
+                      }
+                   }
                   }
+                }
                }
-            }
-         }
-      }
-   }
    return Combination;
 }
 
